4f3bc3d0f4abc3d369d68cd1fec06b5c1f447fab
sraxapvd <siradadeelom@gmail.com>
2026-08-14 14:43:02 +0700

    Delete STUDLY directory

--- STUDLY/backend/store.js
+++
@@ -1,7 +0,0 @@
-const fs=require('fs');
-const path=require('path');
-const file=path.join(__dirname,'data','database.json');
-function read(){return JSON.parse(fs.readFileSync(file,'utf8'));}
-function write(db){fs.writeFileSync(file,JSON.stringify(db,null,2));}
-function update(fn){const db=read(); const result=fn(db); write(db); return result;}
-module.exports={read,write,update};
